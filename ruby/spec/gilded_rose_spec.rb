d487a22748e60a3f763e57973bfa421179fa707a
Shaneil Clarke <shaneilske@yahoo.com>
2019-02-14 12:33:44 +0000

    added spec test for quality decreasing when sell in passed

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -3,11 +3,29 @@ require 'gilded_rose'
 describe GildedRose do
   
   describe "#update_quality" do
+    # let(:item) { Item.new() }
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
+    end 
+    context "item is not Aged Brie, Backstage passes or Sulfuras" do
+      it 'quality decreases when sell in has passed' do
+        items = [Item.new('+5 Dexterity Vest', 10, 20)]
+        rose = GildedRose.new(items)
+        expect { rose.update_quality }.to change { items[0].quality }.by(-1)
+        expect { rose.update_quality }.to change { items[0].sell_in }.by(-1)
+      end
+    end  
+    context "Aged Brie" do
+      it 'quality increases the older it gets' do
+        items = [Item.new('Aged Brie', 2, 0)]
+        rose = GildedRose.new(items)
+        expect { rose.update_quality }.to change { items[0].quality }.by(1)
+      end
     end
-  end
+    end
+    
+    
   
 end
--- ruby/spec/item_spec.rb
+++ ruby/spec/item_spec.rb
@@ -3,15 +3,14 @@ require './lib/item'
 describe Item do
   let(:item) { Item.new('Aged Brie', 2, 0) }
   it 'knows the name of an item' do
-    expect(item.name).to eq 'Aged Brie'  
+    expect(item.name).to eq('Aged Brie')
   end
   it 'knows the sell_in of an item' do
-    expect(item.sell_in).to eq 2  
+    expect(item.sell_in).to eq(2)
   end
   it 'knows the quality of an item' do
-    expect(item.quality).to eq 0  
+    expect(item.quality).to eq(0)  
   end
-
   it 'returns a string about item' do
     expect(item.to_s).to eq('Aged Brie, 2, 0')  
   end
