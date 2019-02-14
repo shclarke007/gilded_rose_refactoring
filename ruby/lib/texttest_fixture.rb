dac09f8edb9f6805fa90a0b06076b3943d7dd72d
Shaneil Clarke <shaneilske@yahoo.com>
2019-02-14 14:26:25 +0000

    added sulfuras spec

--- ruby/lib/texttest_fixture.rb
+++ ruby/lib/texttest_fixture.rb
@@ -8,7 +8,7 @@ items = [
   Item.new(name = "Aged Brie", sell_in = 2, quality = 0),
   Item.new(name = "Elixir of the Mongoose", sell_in = 5, quality = 7),
   Item.new(name = "Sulfuras, Hand of Ragnaros", sell_in = 0, quality = 80),
-  Item.new(name = "Sulfuras, Hand of Ragnaros", sell_in = -1, quality = 80),
+  Item.new(name = "Sulfuras", sell_in = -1, quality = 80),
   Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 15, quality = 20),
   Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 10, quality = 49),
   Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 5, quality = 49),
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,31 +1,45 @@
 require 'gilded_rose'
+require 'item'
 
 describe GildedRose do
   
   describe "#update_quality" do
-    # let(:item) { Item.new() }
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end 
-    context "item is not Aged Brie, Backstage passes or Sulfuras" do
-      it 'quality decreases when sell in has passed' do
+
+    context "Not Aged Brie, Backstage passes or Sulfuras, Hand of Ragnaros" do
+      it 'quality decreases when sell_in has passed' do
         items = [Item.new('+5 Dexterity Vest', 10, 20)]
         rose = GildedRose.new(items)
         expect { rose.update_quality }.to change { items[0].quality }.by(-1)
         expect { rose.update_quality }.to change { items[0].sell_in }.by(-1)
       end
-    end  
-    context "Aged Brie" do
-      it 'quality increases the older it gets' do
-        items = [Item.new('Aged Brie', 2, 0)]
+    end 
+
+    context "Aged Brie & Backstage passes to a TAFKAL80ETC concert" do
+      it 'quality increases when sell_in approaches' do
+        items = [Item.new('Aged Brie', 2, 0),
+                Item.new('Backstage passes to a TAFKAL80ETC concert', 15, 20)]
         rose = GildedRose.new(items)
         expect { rose.update_quality }.to change { items[0].quality }.by(1)
+        expect { rose.update_quality }.to change { items[1].quality }.by(1)
       end
     end
+
+    context "Sulfuras, Hand of Ragnaros" do
+      items = [Item.new('Sulfuras, Hand of Ragnaros', 0, 80)]
+      rose = GildedRose.new(items)
+      it 'quality never decreses' do
+        expect { rose.update_quality }.to change { items[0].quality }.by(0)
+      end
+      it 'never has to be sold' do
+        expect { rose.update_quality }.to change { items[0].sell_in }.by(0)
+      end    
     end
     
-    
-  
+
+    end 
 end
